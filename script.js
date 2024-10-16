07cbd3f2008b744be81b2a1b1f4a01b9054b24c2
Talha Hussain <talhahussain.splitarts@gmail.com>
2024-10-16 15:54:01 +0500

    code formatted

--- script.js
+++ script.js
@@ -44,12 +44,12 @@ const GameController = {
   winCombinations: [
     [0, 1, 2],
     [3, 4, 5],
-    [6, 7, 8], // Rows
+    [6, 7, 8], 
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8], // Columns
+    [2, 5, 8], 
     [0, 4, 8],
-    [2, 4, 6], // Diagonals
+    [2, 4, 6], 
   ],
 
   startGame(name1, name2) {
